4f99023b927a14c2b5743cb0ed0066691d1dcb1b
r0nharley <rharley77@gmail.com>
2021-06-09 18:03:38 -0400

    clean_up

--- DataDiff/automation/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-# allows the inclusion of libraries not underneath this file structure
-sys.path.append(os.path.realpath(os.path.join(__file__, "../../..")))
--- DataDiff/build_cube.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ssh -ttf ec2-user@provisioner.test.rentlytics.com sudo chef-client -o provisioner::build-deploy
